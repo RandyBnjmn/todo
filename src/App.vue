f7227d8b2a25dcae2b9149c69296bce51d4cd5ad
Randy Benjamin Castro <61950643+RandyBnjmn@users.noreply.github.com>
2022-02-15 06:18:40 -0800

    sweetalert

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <v-spacer></v-spacer>
+      <v-spacer> </v-spacer>
     </v-app-bar>
 
     <v-main>
-      <v-container fluid class="my-5">
+      <v-container class="my-5">
         <v-row class="mx-16">
-          <v-col>
+          <v-col class="text-end">
             <v-dialog v-model="dialog" max-width="600">
               <template v-slot:activator="{ on, attrs }">
                 <v-btn color="primary" dark v-bind="attrs" v-on="on">
@@ -62,10 +62,7 @@
                       </v-col>
 
                       <v-col cols="6">
-                        <v-checkbox
-                          label="Completada"
-                          v-model="tarea.completada"
-                        >
+                        <v-checkbox label="Completada" v-model="tarea.completada">
                         </v-checkbox>
                       </v-col>
                     </v-row>
@@ -73,12 +70,8 @@
                 </v-card-text>
                 <v-card-actions>
                   <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeDialog">
-                    Cerrar
-                  </v-btn>
-                  <v-btn color="blue darken-1" text @click="guardar">
-                    Guardar
-                  </v-btn>
+                  <v-btn color="blue darken-1" text @click="closeDialog"> Cerrar </v-btn>
+                  <v-btn color="blue darken-1" text @click="guardar"> Guardar </v-btn>
                 </v-card-actions>
               </v-card>
             </v-dialog>
@@ -87,18 +80,22 @@
 
         <v-row>
           <v-col cols="12" md="4" v-for="tarea in tareas" :key="tarea.id">
-            <Card :tarea="tarea" @completada="editar" @delete="eliminar" />
+            <Card :tarea="tarea" @completada="editar" @delete="eliminar" @edit="editar" />
           </v-col>
         </v-row>
+        <!-- <div class="text-center">
+          <v-pagination v-model="page" :length="" circle></v-pagination>
+        </div> -->
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  import Card from '@/components/Card.vue'
-  import { mapActions } from 'vuex'
-  import apiTareas from '@/api/apiTareas'
+  import Card from '@/components/Card.vue';
+  import { mapActions } from 'vuex';
+  import apiTareas from '@/api/apiTareas';
+  import Swal from 'sweetalert2';
   export default {
     name: 'App',
     components: {
@@ -107,6 +104,7 @@
 
     data() {
       return {
+        page: 1,
         dialog: false,
         datePicker: false,
         tareas: [],
@@ -116,96 +114,138 @@
           completada: false,
           fecha: null,
         },
-      }
+      };
     },
     methods: {
       ...mapActions(['getTareas']),
 
       closeDialog() {
-        this.dialog = false
-        this.clear()
+        this.dialog = false;
+        this.clear();
       },
       clear() {
-        this.tarea.titulo = ''
-        this.tarea.descripcion = ''
-        this.tarea.completada = false
-        this.tarea.fecha = null
+        this.tarea.titulo = '';
+        this.tarea.descripcion = '';
+        this.tarea.completada = false;
+        this.tarea.fecha = null;
       },
 
       async getTareas() {
-        const { data } = await apiTareas.get('/tareas.json')
+        const { data } = await apiTareas.get('/tareas.json');
 
         for (let id of Object.keys(data)) {
           this.tareas.push({
             id,
             ...data[id],
-          })
+          });
         }
       },
 
       async guardar() {
         if (this.tarea.id) {
-          const { titulo, fecha, descripcion, completada } = this.tarea
+          new Swal({
+            title: 'Espere por favor',
+            allowOutsideClick: false,
+          });
+          Swal.showLoading();
+          const { titulo, fecha, descripcion, completada } = this.tarea;
 
           const datos = {
             titulo,
             fecha,
             descripcion,
             completada,
-          }
-          const { data } = await apiTareas.put(
-            `/tareas/${this.tarea.id}.json`,
-            datos
-          )
+          };
+          const { data } = await apiTareas.put(`/tareas/${this.tarea.id}.json`, datos);
 
-          const idx = this.tareas
-            .map((tarea) => tarea.id)
-            .indexOf(this.tarea.id)
+          const idx = this.tareas.map((tarea) => tarea.id).indexOf(this.tarea.id);
           const dto = {
             id: this.tarea.id,
             titulo: data.titulo,
             descripcion: data.descripcion,
             fecha: data.fecha,
             completada: data.completada,
-          }
-          this.tareas[idx] = dto
-          this.clear()
-          this.dialog = false
+          };
+          this.tareas[idx] = dto;
+          this.clear();
+          this.dialog = false;
+          Swal.fire({
+            title: 'Actulizada',
+            text: 'Tarea actualizadda con exito',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 2000,
+          });
         } else {
-          const { data } = await apiTareas.post('/tareas.json', this.tarea)
-          const id = data.name
-          this.dialog = false
+          new Swal({
+            title: 'Espere por favor',
+            allowOutsideClick: false,
+          });
+          Swal.showLoading();
+          const { data } = await apiTareas.post('/tareas.json', this.tarea);
+          const id = data.name;
+          this.dialog = false;
           this.tareas.push({
             id,
             ...this.tarea,
-          })
-          this.clear()
+          });
+          this.clear();
+          Swal.fire({
+            title: 'Agregada',
+            text: 'Tarea agregada con exito',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 2000,
+          });
         }
       },
 
       editar(id) {
-        const idx = this.tareas.map((tarea) => tarea.id).indexOf(id)
-        const tarea = this.tareas[idx]
+        const idx = this.tareas.map((tarea) => tarea.id).indexOf(id);
+        const tarea = this.tareas[idx];
 
         //evitando reactividad
-        this.tarea.id = id
-        this.tarea.titulo = tarea.titulo
-        this.tarea.descripcion = tarea.descripcion
-        this.tarea.fecha = tarea.fecha
-        this.tarea.completada = tarea.completada
+        this.tarea.id = id;
+        this.tarea.titulo = tarea.titulo;
+        this.tarea.descripcion = tarea.descripcion;
+        this.tarea.fecha = tarea.fecha;
+        this.tarea.completada = tarea.completada;
 
         if (tarea.titulo) {
-          this.dialog = true
+          this.dialog = true;
         }
       },
 
-      eliminar(id) {
-        console.log(id)
+      async eliminar(id) {
+        console.log(id);
+        const { isConfirmed } = await Swal.fire({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!',
+        });
+
+        if (!isConfirmed) return;
+
+        new Swal({
+          title: 'Espere por favor',
+          allowOutsideClick: false,
+        });
+        Swal.showLoading();
+
+        await apiTareas.delete(`/tareas/${id}.json`);
+
+        this.tareas = this.tareas.filter((tarea) => tarea.id !== id);
+
+        Swal.fire('Eliminada', 'Tarea eliminada con exito', 'success', 2000);
       },
     },
 
     created() {
-      this.getTareas()
+      this.getTareas();
     },
-  }
+  };
 </script>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,10 @@
 <template>
+  <!-- @dblclick="$emit('completada', tarea.id)" -->
   <v-card
     outlined
     elevation="2"
     class="mx-2"
     :class="tarea.completada ? 'green lighten-5' : 'red lighten-5'"
-    @dblclick="$emit('completada', tarea.id)"
   >
     <v-card-title>{{ tarea.titulo }}</v-card-title>
 
@@ -15,23 +15,41 @@
       </template>
       <span>{{ tarea.descripcion }}</span>
     </v-tooltip>
-    <v-card-actions style="justify-content: space-between">
+    <!-- style="justify-content: space-between" -->
+    <v-card-actions>
       <span> {{ isCompleted }}</span>
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            @click="$emit('delete', tarea.id)"
-            :icon="true"
-            color="red"
-            dark
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon color="red">delete</v-icon>
-          </v-btn>
-        </template>
-        <span>Eliminar</span>
-      </v-tooltip>
+      <v-row justify="end">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              @click="$emit('edit', tarea.id)"
+              :icon="true"
+              color="warning"
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon color="warning">edit</v-icon>
+            </v-btn>
+          </template>
+          <span>Editar</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              @click="$emit('delete', tarea.id)"
+              :icon="true"
+              color="red"
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon color="red">delete</v-icon>
+            </v-btn>
+          </template>
+          <span>Eliminar</span>
+        </v-tooltip>
+      </v-row>
     </v-card-actions>
   </v-card>
 </template>
@@ -45,13 +63,13 @@
     },
     computed: {
       isCompleted() {
-        return this.tarea.completada ? 'Completada' : 'Pendiente'
+        return this.tarea.completada ? 'Completada' : 'Pendiente';
       },
       descripcion() {
-        return this.tarea.descripcion.length > 50
-          ? this.tarea.descripcion.substring(0, 50) + '...'
-          : this.tarea.descripcion
+        return this.tarea.descripcion.length > 40
+          ? this.tarea.descripcion.substring(0, 40) + '...'
+          : this.tarea.descripcion;
       },
     },
-  }
+  };
 </script>
